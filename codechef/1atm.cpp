b6f11c31d7cbfd0eb2dbee4fe6da9599e526bb2f
Anant <aakashbharduajramanuj@gmail.com>
2021-12-18 18:16:56 +0530

    practice on codechef

--- codechef/1atm.cpp
+++ codechef/1atm.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    
+    int a;
+    float b;
+    cin >> a;
+    cin >> b;
+    if(a%5 == 0 && b>=(a + 0.5)){
+        cout <<fixed<<setprecision(2)<<b - (a + 0.5);
+
+    }else{
+        cout <<fixed<<setprecision(2)<<b;
+    }
     return 0;
 }
