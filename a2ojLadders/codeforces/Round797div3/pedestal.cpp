f11a63bf48149792b27251c6d487369b72852cf4
Anant Chaudhary <aakashbharduajramanuj@gmail.com>
2022-06-11 03:06:08 +0530

    m,mnkjn

---
+++ a2ojLadders/codeforces/Round797div3/pedestal.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heights(int a){
+    if(a%3 == 0){
+        
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    int a;
+
+    while(t--){
+        cin >> a;
+        heights(a);
+        
+    }
+    return 0;
+}
